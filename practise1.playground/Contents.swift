f4b5b9e7c0338ce5dcf685926b2777f8e350fff8
SreehariMax <sreeharimax@gmail.com>
2023-11-14 23:38:54 +0530

    initial

--- practise1.playground/Contents.swift
+++ practise1.playground/Contents.swift
@@ -1,17 +1,40 @@
 import UIKit
 
 
-func reverseNumber(_ number: Int) -> Int {
-    var result = 0
-    var tempNumber = number
-    while tempNumber != 0 {
-        let digit = tempNumber % 10
-        result = result * 10 + digit
-        tempNumber /= 10
-    }
-    return result
-}
-let inputNumber = 12345
-let reversedNumber = reverseNumber(inputNumber)
-print("Reverse of \(inputNumber): \(reversedNumber)")
+//reverse numbers
 
+
+//func reverseNumber(_ number: Int) -> Int {
+//    var result = 0
+//    var tempNumber = number
+//    while tempNumber != 0 {
+//        let digit = tempNumber % 10
+//        result = result * 10 + digit
+//        tempNumber /= 10
+//    }
+//    return result
+//}
+//let inputNumber = 12345
+//let reversedNumber = reverseNumber(inputNumber)
+//print("Reverse of \(inputNumber): \(reversedNumber)")
+
+//factorial
+
+
+func factorial(_ n: Int) -> Int {
+    guard n >= 0 else {
+        return -1
+    }
+    if n == 0 || n == 1 {
+        return 1
+    } else {
+        return n * factorial(n - 1)
+    }
+}
+let number = 5
+let result = factorial(number)
+if result != -1 {
+    print("Factorial of \(number): \(result)")
+} else {
+    print("Factorial is not defined for negative numbers.")
+}
