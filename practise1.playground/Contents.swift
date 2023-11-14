1fff995bd86d8b3f72ec18126bfc42eb09a476ca
SreehariMax <sreeharimax@gmail.com>
2023-11-14 23:41:32 +0530

    second

--- practise1.playground/Contents.swift
+++ practise1.playground/Contents.swift
@@ -21,20 +21,42 @@ import UIKit
 //factorial
 
 
-func factorial(_ n: Int) -> Int {
-    guard n >= 0 else {
-        return -1
+//func factorial(_ n: Int) -> Int {
+//    guard n >= 0 else {
+//        return -1
+//    }
+//    if n == 0 || n == 1 {
+//        return 1
+//    } else {
+//        return n * factorial(n - 1)
+//    }
+//}
+//let number = 5
+//let result = factorial(number)
+//if result != -1 {
+//    print("Factorial of \(number): \(result)")
+//} else {
+//    print("Factorial is not defined for negative numbers.")
+//}
+
+//prime number
+
+
+func isPrime(_ n: Int) -> Bool {
+    guard n > 1 else {
+        return false
     }
-    if n == 0 || n == 1 {
-        return 1
-    } else {
-        return n * factorial(n - 1)
+    for i in 2..<n {
+        if n % i == 0 {
+            return false
+        }
     }
+    return true
 }
-let number = 5
-let result = factorial(number)
-if result != -1 {
-    print("Factorial of \(number): \(result)")
+let number = 17
+if isPrime(number) {
+    print("\(number) is a prime number.")
 } else {
-    print("Factorial is not defined for negative numbers.")
+    print("\(number) is not a prime number.")
 }
+
